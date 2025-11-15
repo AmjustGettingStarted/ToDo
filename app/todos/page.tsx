f362974e75e2235083cc1bf6d38225f31c6a95c2
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-11-15 22:19:25 +0530

    feat(todos): add simple layout

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,9 +1,15 @@
-const page = () => {
+import { getTodos, createTodo } from "@/app/actions/todos";
+import AddTodoForm from "./add-todo-form";
+import TodoList from "./todo-list";
+
+export default async function TodosPage() {
+  const todos = await getTodos();
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="max-w-md mx-auto mt-10 space-y-6">
+      <h1 className="text-2xl font-bold">Todo Test</h1>
+      <AddTodoForm />
+      <TodoList todos={todos} />
     </div>
   );
-};
-
-export default page;
+}
